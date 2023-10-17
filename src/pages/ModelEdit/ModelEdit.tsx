703f2894e80f99b4607ab13d3cbf2ea6e5f4b083
Rain <silverain1qw@gmail.com>
2023-10-18 02:52:39 +0300

    Search bar

--- src/pages/ModelEdit/ModelEdit.tsx
+++ src/pages/ModelEdit/ModelEdit.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import {useParams} from "react-router-dom";
 import {useGetModelQuery} from "../../app/api/ModelsApi";
 import styles from './ModelEdit.module.css'
 import {ModelObject} from "../../components/ModelObject/ModelObject";
-import {Alert} from "@mui/material";
-
-
+import {Alert, InputAdornment, TextField, IconButton} from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
 
 export const ModelEdit = () => {
     const {modelName} = useParams();
@@ -14,6 +13,7 @@ export const ModelEdit = () => {
 
     const [success, setSuccess] = React.useState(false)
     const [error, setError] = React.useState(false)
+    const [searchTerm, setSearchTerm] = React.useState('');
 
     useEffect(() => {
         setTimeout(() => {
@@ -22,29 +22,72 @@ export const ModelEdit = () => {
         }, 5000);
     }, [success, error]);
 
+
+    const handleSearchChange = (event: any) => {
+        setSearchTerm(event.target.value);
+    };
+
+
     return (
         <>
             {
                 success && (
                     <Alert onClose={() => {
-                        setSuccess(false)}}>Операция выполнена успешно!</Alert>
+                        setSuccess(false)
+                    }}>Операция выполнена успешно!</Alert>
                 )
             }
             {
                 error && (
-                    <Alert severity={'error'} onClose={() => {setError(false)}}>Произошла ошибка! Для более подробной информации проверьте консоль</Alert>
+                    <Alert severity={'error'} onClose={() => {
+                        setError(false)
+                    }}>Произошла ошибка! Для более подробной информации проверьте консоль</Alert>
                 )
             }
-        <div className={styles.objectsListContainer}>
-            <div className={styles.objectsListWrapper}>
-                {data && data.map((el: any) => {
-                    return (
-                        <ModelObject modelName={modelName} id={el.id} name={el.name === undefined ? el.title : el.name} setError={setError} setSuccess={setSuccess}/>
-                    );
-                })}
-            </div>
-        </div>
-        </>
+            <div className={styles.searchBarContainer}>
+                <TextField
+                    label="Поиск"
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <IconButton>
+                                    <SearchIcon/>
+                                </IconButton>
+                            </InputAdornment>
+                        ),
+                    }}
+                />
 
+            </div>
+            <div className={styles.objectsListContainer}>
+                <div className={styles.objectsListWrapper}>
+                    {data &&
+                        data
+                            .filter((el: any) => {
+                                const nameToCheck = el.name || '';
+                                const titleToCheck = el.title || '';
+                                return (
+                                    nameToCheck.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                                    titleToCheck.toLowerCase().includes(searchTerm.toLowerCase())
+                                );
+                            })
+                            .map((el: any) => {
+                                return (
+                                    <ModelObject
+                                        key={el.id}
+                                        modelName={modelName}
+                                        id={el.id}
+                                        name={el.name === undefined ? el.title : el.name}
+                                        setError={setError}
+                                        setSuccess={setSuccess}
+                                    />
+                                );
+                            })}
+                </div>
+            </div>
+
+        </>
     );
 };
