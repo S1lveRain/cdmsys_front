90d8cdbf9466dbadc11eac9447184f8f83dda497
Rain <silverain1qw@gmail.com>
2023-10-25 16:27:31 +0300

    Homescreen loading

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,23 +3,44 @@ import {Model} from '../../components/Model/Model';
 import styles from './Home.module.css'
 import {useGetModelsQuery} from "../../app/api/ModelsApi";
 import Stack from '@mui/material/Stack';
+import {Skeleton} from "@mui/material";
+
 
 export const Home = () => {
-    const {data} = useGetModelsQuery('');
+    const {data, isLoading} = useGetModelsQuery('');
+
+    let arr = Array.from({ length: 6 }, (_, idx) => `${++idx}`)
+
     return (
         <div className={styles.homeContainer}>
             <h3>Все модели</h3>
-            {data && data.map((el: any) => {
-                return (
-                    <>
+            {
+                isLoading ? (
+                    <div className={styles.loadingContainer}>
                         <Stack spacing={2}>
-                            <div className={styles.homeWrapper}>
-                                <Model label={el.modelLabel} name={el.modelName} count={el.count}/>
-                            </div>
+                            {
+                                arr.map(() => {
+                                    return(<Skeleton style={{height: 100, width: '80vw'}}/>)
+                                })
+                            }
                         </Stack>
-                    </>
+                    </div>
+                ) : (
+                    <div>
+                        {data && data.map((el: any) => {
+                            return (
+                                <>
+                                    <Stack spacing={2}>
+                                        <div className={styles.homeWrapper}>
+                                            <Model label={el.modelLabel} name={el.modelName} count={el.count}/>
+                                        </div>
+                                    </Stack>
+                                </>
+                            )
+                        })}
+                    </div>
                 )
-            })}
+            }
         </div>
     );
 };
